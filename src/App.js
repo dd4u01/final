e2ed93b290db8f528be2331e8b8ff0194589bffc
dd4u01 <dd4u01@gmail.com>
2023-06-15 13:47:03 +0900

    final commit2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import styled from "styled-components"
+import {useState} from "react";
+
+const bgColor =(props)=> styled.div`
+  width: 100vw;
+  height: 100vw;
+`;
 
 function App() {
+  const [text, setText] = useState('색깔 값을 입력해주세요');
+
+  const onchangeText =(e) =>{
+    setText(e.target.value);
+    console.log(e.target.value);
+  }
   return (
-    <div className="App">
-      
-    </div>
+      <bgColor style={{backgroundColor:"red"}}>
+        <h3>Background Color : {text}</h3><input onChange = {onchangeText}/>
+      </bgColor>
   );
-}
+};
+
+
 
 export default App;
